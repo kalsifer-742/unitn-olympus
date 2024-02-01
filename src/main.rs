96b08647caa5484c2b66287439381117ce9d6ae2
Kalsifer <kalsifer742@gmail.com>
2024-02-01 18:35:18 +0100

    rendering world with textures

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,16 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() {
     let gui = Rc::new(RefCell::new(GUI::default()));
-    let mut world_generator = MyWorldGen::new_param(25, 5, 5, 5, true, false, 5);
+    let world_size = 25;
+    let mut world_generator = MyWorldGen::new_param(world_size, 5, 5, 5, true, false, 5);
     let robot = DummyRobot::new(Robot::default(), gui.clone());
     let mut game_logic = GameLogic::new(
         Runner::new(Box::new(robot), &mut world_generator).expect("Error creating runner")
     );
 
-    gui.borrow().init();
+    gui.borrow_mut().init(world_size);
+    let mut last_time = 0.0;
+
     loop {
         //Background
         GUI::draw_background();
@@ -37,8 +40,17 @@ async fn main() {
         //Camera
         set_camera(gui.borrow_mut().camera.get_actual_camera());
 
-        game_logic.tick();
+        let current_time = get_time();
+        
+        if current_time - last_time > 0.2 {
+            game_logic.tick();
+            last_time = current_time;
+        }
+        
         gui.borrow().draw_world();
+
+        set_default_camera();
+        draw_text(format!("FPS: {}", get_fps()).as_str(), 0., 16., 32., ORANGE);
 
         next_frame().await
     }
