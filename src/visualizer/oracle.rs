9e9a529664fa45b7de9ceda463ebdcc0bd038f65
Kalsifer <kalsifer742@gmail.com>
2024-02-11 15:52:10 +0100

    added tick_time

--- src/visualizer/oracle.rs
+++ src/visualizer/oracle.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
 use robotics_lib::interface::{get_score, robot_map};
 use robotics_lib::runner::Runnable;
-use robotics_lib::world::environmental_conditions::EnvironmentalConditions;
+use robotics_lib::world::environmental_conditions::{EnvironmentalConditions, WeatherType};
 use robotics_lib::world::World;
 
 use crate::visualizer::VisualizerProps;
@@ -22,13 +25,33 @@ use crate::visualizer::VisualizerProps;
 //     fn offering_to_the_gods(&self, oracle: &mut Oracle, r_props: RendererProps, g_props: GUIProps);
 // }
 
-pub struct Oracle {
-    props: VisualizerProps
+pub struct Oracle<'a> {
+    props: VisualizerProps<'a>
 }
 
-impl Oracle {
-    pub fn get_props(&self) -> &VisualizerProps {
-        &self.props
+impl<'a> Oracle<'a> {
+    pub fn new(tick_time: &'a mut f32) -> Self {
+        Self {
+            props: VisualizerProps {
+                explored_world_map: vec![vec![None]],
+                robot_coordinates: (0, 0),
+                robot_energy: 0,
+                robot_backpack_contents: HashMap::default(),
+                robot_backpack_size: 0,
+                discoverable_tiles: usize::MAX,
+                robot_score: 0.0,
+                weather: EnvironmentalConditions::new(
+                    &vec![WeatherType::Sunny],
+                    0,
+                    0
+                ).unwrap(),
+                tick_time,
+            }
+        }
+    }
+
+    pub fn get_props(&'a mut self) -> &'a mut VisualizerProps {
+        &mut self.props
     }
 
     pub fn update_props(&mut self, robot: & impl Runnable, world: &mut World) {
@@ -44,12 +67,8 @@ impl Oracle {
     pub fn update_weather(&mut self, weather: EnvironmentalConditions) {
         self.props.weather = weather;
     }
-}
 
-impl Default for Oracle {
-    fn default() -> Self {
-        Self {
-            props: Default::default()
-        }
+    pub fn update_tick(&'a mut self, tick_time: &'a mut f32) {
+        self.props.tick_time = tick_time;
     }
 }
